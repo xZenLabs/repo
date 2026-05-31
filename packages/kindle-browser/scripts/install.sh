9a597dfd733da148b763b73b6b9a6564a43a1cbd
Anthony Gress <the.techx.guy@gmail.com>
2026-05-31 11:10:21 -0400

    update kindle browser

--- packages/kindle-browser/scripts/install.sh
+++ packages/kindle-browser/scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-curl -fSL --progress-bar -o /mnt/us/documents/ZenBrowser.sh \
-  https://zen-labs-x.github.io/repo/packages/kindle-browser/assets/ZenBrowser.sh
+curl -fSL --progress-bar -o /mnt/us/documents/Browser.sh \
+  https://zen-labs-x.github.io/repo/packages/kindle-browser/assets/Browser.sh
 
 exit 0
